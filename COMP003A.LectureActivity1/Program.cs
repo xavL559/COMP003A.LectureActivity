70493fcc00308778b607078de8a7ad3fcdc47920
Xavier Leftridge <111714709+xavL559@users.noreply.github.com>
2023-01-18 19:06:03 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,20 @@
+﻿// this is a single-line comment
+/*This is a multi-line comment
+ *Author: Xavier Leftridge
+ *Course: Comp-003A
+ *Purpose: Lecture Activity
+ */
+namespace COMP003A.LectureActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("This is a string");
+
+            Console.WriteLine("Xavier Leftridge");
+        }
+    }
+}
